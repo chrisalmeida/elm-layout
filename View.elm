7212c40c37155357dea9f43448e1d015d49c660c
chrisalmeida <thechrisalmeida@gmail.com>
2016-09-10 15:30:32 -0700

    Update View.elm
    fix tabs

--- View.elm
+++ View.elm
@@ -2,15 +2,16 @@ module View exposing (..)
 import Html exposing (div, text, h1, p, h4)
 
 layout = 
-    div [] [ div[] [ header]
+    div [] [ div[] [ header ]
     , div [] [ body ]
     , div [] [ footer ]
     ]
 
 header =
     h1 [] [ text "I am a Header" ]
+    
 body = 
     p [] [ text "Layout Body" ]
-
+    
 footer = 
     h4 [] [ text "I am a footer" ]
